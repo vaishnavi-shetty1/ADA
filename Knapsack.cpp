1d550d6504ad483865e26093a8705bca7607eb98
Vaishnavi Shetty <shettyvaishnavi871@gmail.com>
2025-06-03 11:33:49 +0530

    Update 0/1Knapsack.cpp

--- Knapsack.cpp
+++ Knapsack.cpp
@@ -1,88 +1,36 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-
+#include <iostream>
+#include <vector>
 using namespace std;
-
-struct Item {
-	int profit;
-	int weight;
-	float profitRatio;
-	float fraction;
-};
-
-bool compareItems(Item a,Item b) {
-	return a.profitRatio > b.profitRatio;
+int knapsack(int W, vector<int>& weights, vector<int>& values, int n) {
+vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+for (int i = 1; i <= n; ++i) {
+for (int w = 0; w <= W; ++w) {
+if (weights[i - 1] <= w) {
+dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i -
+1][w]);
+} else {
+dp[i][w] = dp[i - 1][w];
 }
-
-void fractionalKnapsack(vector<Item> &items, int knCap) {
-	float remCap = knCap, totProfit = 0.0;
-	
-	for(auto &item : items){
-		if(item.weight <= remCap) {
-			item.fraction = 1.0;
-			remCap -= item.weight;
-			totProfit += item.profit;
-		}else {
-			item.fraction = remCap / item.weight;
-			totProfit += item.profit * item.fraction;
-			break;
-		}
-	}
-	
-	cout << "\n Fractional Knapsack Solution:\n";
-	cout << "Item\tWeight\tProfit\tFraction Chosen\n";
-	for(size_t i = 0; i < items.size(); i++) {
-		cout << i + 1 << "\t" << items[i].weight << "\t" << items[i].profit << "\t" << items[i].fraction << endl;
-	}
-	cout << "\nTotal Profit (Fractional): " << totProfit << endl;
 }
-
-void zeroOneKnapsack(vector<Item> &items, int knCap) {
-	int remCap = knCap, totProfit = 0;
-	
-	for(auto &item : items) {
-		if(item.weight <= remCap) {
-			item.fraction = 1.0;
-			remCap -= item.weight;
-			totProfit += item.profit;
-		}else {
-			item.fraction = 0.0;
-		}
-	}
-	
-	cout << "\n0/1 Knapsack Solution (Greedy, may be suboptimal):\n";
-	cout << "Item\tWeight\tProfit\tTaken(1 = Yes, 0 = No)\n";
-	for(size_t i = 0; i < items.size(); i++) {
-		cout << i + 1 << "\t" << items[i].weight << "\t" << items[i].profit << "\t" << (items[i].fraction == 1.0 ? "1" : "0") << endl;
-	}
-	cout << "\nTotal Profit (0\1 Greedy): " << totProfit << endl;
 }
-
+return dp[n][W];
+}
 int main() {
-	int iNum, knCap;
-	vector<Item> items;
-	
-	cout << "Enter the number of items:";
-	cin >> iNum;
-	cout << "Enter Knapsack capacity:";
-	cin >> knCap;
-	
-	for(int i = 0; i < iNum; i++) {
-		Item item;
-		cout << "Enter profit:";
-		cin >> item.profit;
-		cout << "Enter weight:";
-		cin >> item.weight;
-		item.profitRatio = (float)item.profit / item.weight;
-		item.fraction = 0.0f;
-		items.push_back(item);
-	}
-	
-	sort(items.begin(), items.end(), compareItems);
-	
-	fractionalKnapsack(items, knCap);
-	zeroOneKnapsack(items, knCap);
-	
-	return 0;
+int n, W;
+cout << "Enter number of items: ";
+cin >> n;
+vector<int> values(n), weights(n);
+cout << "Enter values of items:\n";
+for (int i = 0; i < n; ++i) {
+cin >> values[i];
+}
+cout << "Enter weights of items:\n";
+for (int i = 0; i < n; ++i) {
+cin >> weights[i];
+}
+cout << "Enter capacity of knapsack: ";
+cin >> W;
+int max_value = knapsack(W, weights, values, n);
+cout << "Maximum value in knapsack = " << max_value << endl;
+return 0;
 }
